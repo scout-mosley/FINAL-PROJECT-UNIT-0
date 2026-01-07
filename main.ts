ad18cbff3a68b0846ea51375497bb1f30559c998
Scout Mosley <scout.mosley@stu.ocsb.ca>
2026-01-07 12:39:42 -0500

    deleted extra code

--- main.ts
+++ main.ts
@@ -20,16 +20,6 @@ if (InputA==0) {
     InputA=InputA-1
 }
    
-// A button on off
-    input.onButtonPressed(Button.A, function () {
-if (InputA==0) {
-    pins.digitalWritePin(DigitalPin.P0, 1)
-    InputA=InputA+1
-} else {
-    pins.digitalWritePin(DigitalPin.P0, 0)
-    InputA=InputA-1
-}
-   
 })
 
 
@@ -40,15 +30,6 @@ input.onButtonPressed(Button.B, function () {
         InputB = InputB + 1
     } else {
         pins.digitalWritePin(DigitalPin.P11, 0)
-        InputB = InputB - 1
-    }
-// B button on off
-input.onButtonPressed(Button.B, function () {
-    if (InputB == 0) {
-        pins.digitalWritePin(DigitalPin.P1, 1)
-        InputB = InputB + 1
-    } else {
-        pins.digitalWritePin(DigitalPin.P1, 0)
         InputB = InputB - 1
     }
 
@@ -68,30 +49,4 @@ input.onButtonPressed(Button.AB, function () {
         InputAB = InputAB - 1
     }
 
-// A+B button on off
-input.onButtonPressed(Button.AB, function () {
-    if (InputAB == 0) {
-        pins.digitalWritePin(DigitalPin.P0, 1)
-        pins.digitalWritePin(DigitalPin.P1, 1)
-
-        InputAB = InputAB + 1
-    } else {
-        pins.digitalWritePin(DigitalPin.P0, 0)
-        pins.digitalWritePin(DigitalPin.P1, 0)
-
-        InputAB = InputAB - 1
-    }
-
 })
-    
-
-    
-    if (InputA == 1, InputB == 0) {
-        basic.showString("A-1 B-0")
-    } 
-    if (InputA == 0, InputB == 1) {
-        basic.showString("A-0 B-1")
-    if (InputAB == 1) {
-}
-}
-
