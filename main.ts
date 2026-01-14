df8eeab94b4be81a2208ce1420f2ae07a6d1cb1a
Scout Mosley <scout.mosley@stu.ocsb.ca>
2026-01-14 13:18:35 -0500

    fixed up the spacing

--- main.ts
+++ main.ts
@@ -5,16 +5,12 @@
  * This program works as a half adder
 */
 
-
-
 // startup
 pins.digitalWritePin(DigitalPin.P0, 0)
 pins.digitalWritePin(DigitalPin.P1, 0)
 pins.digitalWritePin(DigitalPin.P8, 0)
 pins.digitalWritePin(DigitalPin.P9, 0)
 basic.showIcon(IconNames.Yes)
-
-
 
 while (true) {
     // A pressed
